9c363b7db043ee0146201564a08afe215e03c219
David Laing <david@davidlaing.com>
2013-02-23 02:42:36 +0000

    Prevent caching ga caching pixel

--- http-server.js
+++ http-server.js
@@ -8,6 +8,9 @@ var app = connect()
     if (req.url == '/ga.gif') {
   		NA.initialize('UA-38642802-1', 'cf-docs-contrib.cloudfoundry.com', function () {
 			  NA.trackPage('GA pixel page', req.headers.referer, function (err, resp) {
+			  	res.setHeader('cache-control', 'max-age=0, no-cache, no-store, must-revalidate');
+			  	res.setHeader('expires', 0);
+			  	res.setHeader('pragma', 'no-cache');
 			    res.end(resp.body);
 			  });
 			});
