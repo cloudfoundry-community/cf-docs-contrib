2b3b9b06dc819bf46776adf8d57c2abc11ec7d86
David Laing <david@davidlaing.com>
2013-02-23 10:36:16 +0000

    Match any .gif as a tracking pixel, and ensure it isn't cached

--- http-server.js
+++ http-server.js
@@ -5,18 +5,22 @@ var app = connect()
   .use(connect.logger())
   .use(connect.static(__dirname + '/build', { maxAge: 0 }))
   .use(function(req, res){
-    if (req.url == '/ga.gif') {
-  		NA.initialize('UA-38642802-1', 'cf-docs-contrib.cloudfoundry.com', function () {
+  	//Any other unmatched .gif request is treated as a tracking pixel
+  	if (req.url.indexOf('.gif') > -1) {
+			NA.initialize('UA-38642802-1', 'cf-docs-contrib.cloudfoundry.com', function () {
 			  NA.trackPage('GA pixel page', req.headers.referer, function (err, resp) {
-			  	res.setHeader('cache-control', 'max-age=0, no-cache, no-store, must-revalidate');
-			  	res.setHeader('expires', 0);
-			  	res.setHeader('pragma', 'no-cache');
+			  	res.setHeader('Cache-control', 'private, no-cache, no-cache=Set-Cookie, proxy-revalidate, max-age=0, no-store, must-revalidate');
+			  	res.setHeader('Cache-directive', 'no-cache');
+			  	res.setHeader('Expires', 'Wed, 17 Sep 1975 21:32:10 GMT');
+			  	res.setHeader('Pragma', 'no-cache');
+			  	res.setHeader('Pragma-directive', 'no-cache');
+					res.setHeader('X-Powered-By', 'NodeJs hamsters');
 			    res.end(resp.body);
 			  });
 			});
-  		return;
-  	}
-  	res.statusCode = 404;
+			return;
+		}
+		res.statusCode = 404;
   	res.end();
   })
   .listen(3001);
