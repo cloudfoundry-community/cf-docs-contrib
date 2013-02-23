a5fb8cc9e16d6d79a74ed5c084ce0f9d80c0276f
David Laing <david@davidlaing.com>
2013-02-23 02:30:23 +0000

    /ga.gif returns google analytics tracking pixel

--- http-server.js
+++ http-server.js
@@ -1,6 +1,20 @@
 var connect = require('connect');
+var NA = require("nodealytics");
+
 var app = connect()
   .use(connect.logger())
   .use(connect.static(__dirname + '/build', { maxAge: 0 }))
+  .use(function(req, res){
+    if (req.url == '/ga.gif') {
+  		NA.initialize('UA-38642802-1', 'cf-docs-contrib.cloudfoundry.com', function () {
+			  NA.trackPage('GA pixel page', req.headers.referer, function (err, resp) {
+			    res.end(resp.body);
+			  });
+			});
+  		return;
+  	}
+  	res.statusCode = 404;
+  	res.end();
+  })
   .listen(3001);
 console.log("Listening on http://%s:%d", app.address().address, app.address().port);
